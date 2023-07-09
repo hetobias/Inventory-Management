fbcd133f0e55d3e3e0e22502da65080573eb4628
Tobias <tobiashe17@gmail.com>
2023-07-09 18:28:09 -0400

    changed mapping for warehouse
    change warehouses to warehouse for request mapping

--- backend/InventoryManagementSystem/src/main/java/com/skillstorm/InventoryManagementSystem/controller/WarehouseController.java
+++ backend/InventoryManagementSystem/src/main/java/com/skillstorm/InventoryManagementSystem/controller/WarehouseController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/warehouses")
+@RequestMapping("/warehouse")
 public class WarehouseController {
 
   private final WarehouseService warehouseService;
