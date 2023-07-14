aea6f21196e56aa3e3e7d145a47b7af423bc53fe
Tobias <tobiashe17@gmail.com>
2023-07-14 03:31:07 -0400

    purge
    deleted unused stuff

--- frontend/inventory management/src/components/InventoryTable/InventoryTableRow.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const InventoryTableRow = () => {
-  return (
-    <div>InventoryTableRow</div>
-  )
-}
-
-export default InventoryTableRow;
--- frontend/inventory management/src/components/WarehouseTable/WarehouseTableRow.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const WarehouseTableRow = () => {
-  return (
-    <div>WarehouseTableRow</div>
-  )
-}
-
-export default WarehouseTableRow;
